0ccb077a037ade4ae9ec38d7dab7d63e14eacce9
wellandrade <leto.l@hotmail.com>
2024-08-08 17:41:27 -0300

    feat: search add fuctionalite

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,12 +4,15 @@ import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import { Sheet, SheetTrigger } from "./ui/sheet";
 import SidebarSheet from "./sidebar-sheet";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Image src="/logo1.png" height={18} width={120} alt={""} />
+        <Link href="/">
+          <Image src="/logo1.png" height={18} width={120} alt={""} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline" className="dark-up">
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Header from "./_components/header";
-import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
-import { SearchIcon } from "lucide-react";
 import Image from "next/image";
-import "./globals.css";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 import { quickSearchOptions } from "./_constants/search";
 import BookingItem from "./_components/booking-item";
+import Search from "./_components/search";
+import Link from "next/link";
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({});
@@ -19,67 +18,69 @@ const Home = async () => {
 
   return (
     <div>
+      {/* header */}
       <Header />
-      {/*INICIO*/}
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Wellington!</h2>
-        <p>Segunda-Feira 05 de Agosto</p>
+        {/* TEXTO */}
+        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
 
-        {/*PESQUISA*/}
-        <div className="mt-6 flex items-center gap-2 ">
-          <Input placeholder="FAÇA UMA BUSCA..." className="text-justify" />
-          <Button>
-            <SearchIcon></SearchIcon>
-          </Button>
+        {/* BUSCA */}
+        <div className="mt-6">
+          <Search />
         </div>
 
-        {/*BUSCA RAPIDA*/}
-
+        {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
-            <Button className="gap-2" variant="secondary" key={option.title}>
-              <Image
-                src={option.imageUrl}
-                width={16}
-                height={16}
-                alt={option.title}
-              />
-              {option.title}
+            <Button
+              className="gap-2"
+              variant="secondary"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Link>
             </Button>
           ))}
         </div>
 
-        {/*BANNER*/}
-        <div className="relative  h-[200px] w-full">
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
           <Image
+            alt="Agende nos melhores com FSW Barber"
             src="/Banner011.png"
             fill
-            className="object-cover rounded-xl"
-            alt={""}
+            className="rounded-xl object-cover"
           />
         </div>
 
-        {/*AGENDAMENTO*/}
-        <div>
-          <BookingItem />
+        {/* AGENDAMENTO */}
+        <BookingItem />
 
-          <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-            Recomendados
-          </h2>
-          <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-            {barbershops.map((barbershop) => (
-              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-            ))}
-          </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
 
-          <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-            populares
-          </h2>
-          <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-            {popularBarbershops.map((barbershop) => (
-              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-            ))}
-          </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
         </div>
       </div>
     </div>
